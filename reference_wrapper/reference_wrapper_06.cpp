0994feb32e11e68500b0ed3cedda390b52b05b9b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-20 18:56:23 +0300

    Update reference_wrapper_06.cpp

--- reference_wrapper/reference_wrapper_06.cpp
+++ reference_wrapper/reference_wrapper_06.cpp
@@ -10,6 +10,6 @@ int main()
 	//auto p = make_pair(a, b);
 	auto p = make_pair(ref(a), ref(b));
 	++p.first, ++p.second;
-	std::cout << "a = " << a << "\n";
-	std::cout << "b = " << b << "\n";
+	std::cout << "a = " << a << '\n';
+	std::cout << "b = " << b << '\n';
 }
