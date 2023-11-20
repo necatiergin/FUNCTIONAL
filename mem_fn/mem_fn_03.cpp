051e3c54344930a4b3a5b59ac447aa3285e0fc4e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-20 18:43:49 +0300

    Create mem_fn_03.cpp

---
+++ mem_fn/mem_fn_03.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+class Nec {
+public:
+	Nec(int x = 0) : mx{ x } {}
+	void print()const
+	{
+		std::cout << mx << ' ';
+	}
+private:
+	int mx;
+};
+
+
+int main()
+{
+	using namespace std;
+
+	vector nvec = { Nec{1}, Nec{7}, Nec{3}, Nec{6}, Nec{2}, Nec{8} };
+
+	for_each(nvec.begin(), nvec.end(), mem_fn(&Nec::print));
+	cout << '\n';
+	for_each(nvec.begin(), nvec.end(), [](const Nec& nec) {nec.print(); });
+	cout << '\n';
+}
