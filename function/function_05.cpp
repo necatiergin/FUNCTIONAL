eeb41d5ca76b70be3548fefc1ceade107a78cd8b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-01 22:48:03 +0300

    Update function_05.cpp

--- function/function_05.cpp
+++ function/function_05.cpp
@@ -19,21 +19,21 @@ using fcallback = std::function<int(int)>;
 
 void func(int x, fcallback fnc)
 {
-	std::cout << "func cagrildi\n";
+	std::cout << "func called\n";
 	fnc(x);
 	std::cout << "------------------\n";
 }
 
 int foo(int x)
 {
-	std::cout << "foo(int x) cagrildi x = " << x << "\n";
+	std::cout << "foo(int x) called x = " << x << '\n';
 	return x * x;
 }
 
 int main()
 {
 	func(10, foo);
-	func(10, [](int a) {std::cout<< "lambda\n"; return a * 2; });
+	func(10, [](int a) {std::cout << "lambda\n"; return a * 2; });
 	func(20, Functor{ 5 });
 	func(20, &Myclass::foo);
 }
