9acbfaf60a3f5ed0e94acca85327cf4c5aabb031
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-01 22:49:16 +0300

    Update function_06.cpp

--- function/function_06.cpp
+++ function/function_06.cpp
@@ -10,16 +10,16 @@ class Functor {
 	int mx;
 public:
 	Functor(int x) : mx{ x } {}
-	int operator()(int val)const 
-	{ 
+	int operator()(int val)const
+	{
 		std::cout << "int Functor::operator()(int)\n";
-		return val * mx; 
+		return val * mx;
 	}
 };
 
 class Myclass {
 public:
-	static int foo(int x) 
+	static int foo(int x)
 	{
 		std::cout << "int Myclass::foo(int)\n";
 		return x * x * x;
@@ -30,7 +30,7 @@ public:
 
 int gf(int x)
 {
-	std::cout << "gf(int x) cagrildi\n";
+	std::cout << "gf(int x) called\n";
 	return x * x;
 }
 
@@ -39,7 +39,7 @@ int main()
 	//vector<function<int(int)>> fcvec;
 	vector<fnc> fcvec;
 
-	fcvec.emplace_back([](int x) 
+	fcvec.emplace_back([](int x)
 		{cout << "lambda\n";  return x * x; });
 	fcvec.emplace_back(&Myclass::foo);
 	fcvec.emplace_back(gf);
