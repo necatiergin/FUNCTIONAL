8e08dedab7f43fa6833225052970fa59035c82d6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-09 10:33:33 +0300

    Update and rename mem_fn/mem_fn_03.cpp to mem_fn_03.cpp

--- mem_fn_03.cpp
+++ mem_fn_03.cpp
@@ -17,12 +17,10 @@ private:
 
 int main()
 {
-	using namespace std;
-
-	vector nvec = { Nec{1}, Nec{7}, Nec{3}, Nec{6}, Nec{2}, Nec{8} };
+	std::vector nvec = { Nec{1}, Nec{7}, Nec{3}, Nec{6}, Nec{2}, Nec{8} };
 
 	for_each(nvec.begin(), nvec.end(), mem_fn(&Nec::print));
-	cout << '\n';
+	std::cout << '\n';
 	for_each(nvec.begin(), nvec.end(), [](const Nec& nec) {nec.print(); });
 	cout << '\n';
 }
