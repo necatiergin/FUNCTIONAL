abb7b9956cd3e5fdbb864dfcdea8471b97f38d0b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-09 13:48:36 +0300

    Update bind_04.cpp

--- bind_04.cpp
+++ bind_04.cpp
@@ -3,14 +3,14 @@
 
 class Myclass {
 public:
-	void func()const
+	void foo()const
 	{
-		std::cout << "Myclass::func()\n";
+		std::cout << "Myclass::foo()\n";
 	}
 
-	void foo(int a)const
+	void bar(int a)const
 	{
-		std::cout << "Myclass::foo(int a) a = " << a << '\n';
+		std::cout << "Myclass::bar(int a) a = " << a << '\n';
 	}
 };
 
@@ -20,10 +20,10 @@ int main()
 
 	Myclass mx;
 
-	auto f1 = std::bind(&Myclass::func, _1);
+	auto f1 = std::bind(&Myclass::foo, _1);
 	f1(mx);
 
-	auto f2 = bind(&Myclass::foo, _1, _2);
+	auto f2 = bind(&Myclass::bar, _1, _2);
 
 	f2(mx, 55);
 }
