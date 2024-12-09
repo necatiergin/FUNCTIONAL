6fb257b5651f7d76c4d0a1f97ad303d3c1395aaf
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-09 13:45:49 +0300

    Update bind_02.cpp

--- bind_02.cpp
+++ bind_02.cpp
@@ -6,7 +6,7 @@ int main()
 	using namespace std::placeholders;
 
 	auto fsum = [](int a, int b, int c) {return a + b + c; };
-	auto f1 = std::bind(fsum, _1, _2, 0);
+	auto fb = std::bind(fsum, _1, _2, 0);
 
-	std::cout << f1(23, 37) << '\n';
+	std::cout << fb(23, 37) << '\n';
 }
