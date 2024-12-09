40ca413caa353001a70c5cbfa21e9459ca49525d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-09 15:40:55 +0300

    Update reference_wrapper_01.cpp

--- reference_wrapper_01.cpp
+++ reference_wrapper_01.cpp
@@ -3,15 +3,14 @@
 
 int main(void)
 {
-	using namespace std;
 	int x = 3;
 	int y = 7;
 
-	//reference_wrapper<int> r = x; //ctad
-	reference_wrapper r = x; //ctad
+	//reference_wrapper<int> r = x; 
+	std::reference_wrapper r = x; //ctad
 	++r;
 	r = y;
 	++r;
 
-	cout << x << y;
+	std::cout << x << y;
 }
