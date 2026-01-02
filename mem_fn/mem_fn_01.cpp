060398f9dbf6772e6c432e636454fccf71c76af7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-01-02 15:36:21 +0300

    Update and rename mem_fn_01.cpp to mem_fn/mem_fn_01.cpp

--- mem_fn/mem_fn_01.cpp
+++ mem_fn/mem_fn_01.cpp
@@ -7,8 +7,10 @@
 
 int main()
 {
-	std::vector<std::string> svec{ "Baran", "Cahit", "Figen", "Turhan", "Mahir", "Haldun", "Galip", "Agah", "Hande", "Sevda",
-	"Su", "Cem", "Kaya", "Tevfik", "Muhsin", "Taner", "Fikret", "Sezai", "Yusuf", "Huseyin", };
+	std::vector<std::string> svec{ "Baran", "Cahit", "Figen", "Turhan", "Mahir", 
+	"Haldun", "Galip", "Agah", "Hande", "Sevda",
+	"Su", "Cem", "Kaya", "Tevfik", "Muhsin", "Taner", 
+	"Fikret", "Sezai", "Yusuf", "Huseyin", };
 
 	transform(svec.begin(), svec.end(), std::ostream_iterator<size_t>{std::cout, " "}, mem_fn(&string::size));
 }
