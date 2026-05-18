d07317acfb919fb6874f64079e9c6d616979e0c9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-05-18 19:00:55 +0300

    Update function_09.cpp

--- function/function_09.cpp
+++ function/function_09.cpp
@@ -19,18 +19,15 @@ public:
 
 int main()
 {
-	using namespace std;
-
 	Myclass m;
 
-	function<int(Myclass&, int)> f1 = &Myclass::foo;
+	std::function<int(Myclass&, int)> f1 = &Myclass::foo;
 	f1(m, 10);
-	function<int(const Myclass&, int)> f2 = &Myclass::bar;
+	std::function<int(const Myclass&, int)> f2 = &Myclass::bar;
 	f2(m, 20);
-	function<int(Myclass*, int)> f3 = &Myclass::foo;
+	std::function<int(Myclass*, int)> f3 = &Myclass::foo;
 	f3(&m, 30);
-	function<int(const Myclass*, int)> f4 = &Myclass::bar;
+	std::function<int(const Myclass*, int)> f4 = &Myclass::bar;
 	f4(&m, 40);
 }
-
 
